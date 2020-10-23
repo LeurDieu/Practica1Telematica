1221ecf8195c5464ebb08c42670e097e605c87da
LeurDieu <axlzbot@gmail.com>
2020-10-22 19:22:39 -0500

    SKRT

--- Client.py
+++ Client.py
@@ -3,6 +3,7 @@ from SocketPacket import sPacket as SP
 import glob
 import sys
 import os
+import pathlib
 
 class Client(SocketHandler):
     def __init__(self, h, p):
@@ -10,6 +11,7 @@ class Client(SocketHandler):
         self.sock.connect((str(self.host), int(self.port)))
         self.id = hex(id(self))
         self.dirs = []
+        self.path = pathlib.Path(__file__).parent.absolute()
     
     def run(self):
         while (True):
@@ -22,6 +24,24 @@ class Client(SocketHandler):
             if (msg == 'send_file'):
                 self.loadFile()
 
+            if (msg == 'create_bucket'):
+                self.createBucket()
+
+            if (msg == 'remove_bucket'):
+                self.removeBucket()
+
+            if (msg == 'lsb'):
+                self.lsb()
+            
+            if (msg == 'lsf'):
+                self.lsf()
+            
+            if (msg == 'df'):
+                self.df()
+
+            if (msg == 'rf'):
+                self.rf()
+
             while (True):
                 res = self.awaitResponse(self.sock)
                 if(self.dictionary(res)):
@@ -32,35 +52,62 @@ class Client(SocketHandler):
         if (comm == 0):
             return True
         if (comm == 1 ):
-            pass
+            self.askParams(data)
+
         if (comm == 2 ):
             self.chooseOption(data)
 
         if (comm == 3 ):
-            pass
+            self.saveFile(data)
+
         if (comm == 4 ):
-            pass
-        if (comm == 5 ):
-            pass
+            self.displayOptions(data.getData(),data.getName())
         return False
+        
+
+    def createBucket(self):
+        self.sendMessage(self.sock, 4)
+
+    def removeBucket(self):
+        self.sendMessage(self.sock, 5)
+
+    def lsb(self):
+        self.sendMessage(self.sock, 8)
+    
+    def lsf(self):
+        self.sendMessage(self.sock, 7)
+    
+    def df(self):
+        self.sendMessage(self.sock, 2)
+
+    def rf(self):
+        self.sendMessage(self.sock, 3)
+
+    def askParams(self, data:SP):
+        self.displayOptions(data.getData(),data.getName())
+        self.sendMessage(self.sock, 6, input("->:"))
+
     def chooseOption(self, data:SP):
-        options = data.getData()
+        self.displayOptions(data.getData(),data.getName())
+        self.sendMessage(self.sock, 6, int(input("->:")))
+    
+    def saveFile(self, data:SP):
+        self.writeFile(self.path,data)
+    
+
+    def displayOptions(self, options, q):
         i = 0
-        print("-> ",data.getName(),":\n")
+        print("-> ",q,":\n")
         for opt in options:
             print("  ", i , " - ", opt)
             i += 1
-        self.sendMessage(self.sock, 6, int(input("->:")))
-    
-    
+
     def loadFile(self):
         self.dirs = glob.glob('*')
         i = 0
         for path in self.dirs:
             print("  ",  i, " - ", path)
             i += 1
-
-        #path = self.dirs[int(input("-> Select the file id:\n-> "))]
         path = self.dirs[int(input("-> Select the file id:\n-> "))]
         data = self.readFile(path)
         self.sendMessage(self.sock, 1, data, path)
--- Server.py
+++ Server.py
@@ -3,6 +3,8 @@ from threading import Thread
 from SocketPacket import sPacket as SP
 import os
 import glob
+import shutil
+from pathlib import PurePath as PP
 
 class Server(SocketHandler):
     def __init__(self, h, p, path):
@@ -47,16 +49,6 @@ class Server(SocketHandler):
             except Exception as e: print(e)
 
     def process(self, client):
-        """
-        while True :
-            try:
-                data = self.recvHeader(client)
-                if(data):
-                    data = self.parseData(data)
-                    self.dictionary(data, client)
-                    #self.saveFile(data)
-            except Exception as e: print(e)
-        """
         while (True):
             data = self.awaitResponse(client)
             self.dictionary(data, client)
@@ -66,49 +58,113 @@ class Server(SocketHandler):
         if (comm == 1 ):
             self.saveFile(data, client)
         if (comm == 2 ):
-            pass
+            self.df(client)
         if (comm == 3 ):
-            pass
+            self.rf(client)
         if (comm == 4 ):
-            pass
+            self.createBucket(client)
         if (comm == 5 ):
-            pass
+            self.removeBucket(client)
+        if (comm == 7):
+            self.lsf(client)
+        if (comm == 8):
+            self.lsb(client)
 
-    def createBucket(self):
+    def createBucket(self, client):
+        self.sendMessage(client, 1, self.buckets, "Select the name of the new bucket")
+        data = self.awaitResponse(client)
+
         if (not self.checkDirectory(self.path)):
             print("->Server path doesn't exists")
         else:
-            name = str(input("-> Select the bucket name:\n-> "))
+            #name = str(input("-> Select the bucket name:\n-> "))
+            name = data.getData()
             path = os.path.join(self.path, name)
             try:  
                 os.mkdir(path)
                 self.buckets.append([name, path])
+
+                self.sendMessage(client, 0)
             except:
                 print("->Bucket already exists")
+    
+    def removeBucket(self, client):
+        self.sendMessage(client, 2, self.buckets, "Select the name of the bucket that you want to remove")
+        data = self.awaitResponse(client)
+
+        if (not self.checkDirectory(self.path)):
+            print("->Server path doesn't exists")
+        else:
+            data = data.getData()
+            try:  
+                shutil.rmtree(self.buckets[data][1])
+                self.buckets.pop(data)
+
+                self.sendMessage(client, 0)
+            except:
+                print("->Wrong Bucket ID")
             
     def checkDirectory(self, path):
         return os.path.isdir(path)
 
     def saveFile(self, data:SP, client):
-        """
-        i = 0
-        for buck in self.buckets:
-            print("  ",  i, " - ", buck[0])
-            i += 1
-        ap = int(input("-> Choose the destination bucket:"))
-        """
         self.sendMessage(client, 2, self.buckets, "Select the bucket")
         ap = self.awaitResponse(client)
 
         self.writeFile(self.buckets[ap.getData()][1], data)
         self.sendMessage(client, 0)
 
-    def ls(self, path):
+    def df(self, client):
+        self.sendMessage(client, 2, self.buckets, "Select the bucket with the file")
+        datab = self.awaitResponse(client)
+
+        dirs = self.ls(self.buckets[datab.getData()][0])
+        self.sendMessage(client, 2, dirs, "Select the file")
+        dataf = self.awaitResponse(client)
+
+        path = dirs[dataf.getData()]
+        purepath = PP(path)
+        data = self.readFile(path)
+        self.sendMessage(client, 3, data, purepath.name)
+        self.sendMessage(client, 0)
+
+    def rf(self, client):
+        self.sendMessage(client, 2, self.buckets, "Select the bucket with the file")
+        datab = self.awaitResponse(client)
+
+        dirs = self.ls(self.buckets[datab.getData()][0])
+        self.sendMessage(client, 2, dirs, "Select the file")
+        dataf = self.awaitResponse(client)
+        
+        path = dirs[dataf.getData()]
+        os.remove(path)
+        self.sendMessage(client, 0)
+
+    def ls(self, name):
+        path = os.path.join(self.path, name)
         dirs = glob.glob(path + '\*')
+        return dirs
+        """
         i = 0
+
         for path in dirs:
             print("  ",  i, " - ", path)
             i += 1
+        """
+
+    def lsb(self, client):
+        self.sendMessage(client, 4, self.buckets, "Buckets")
+        self.sendMessage(client, 0)
+
+    def lsf(self, client):
+        self.sendMessage(client, 2, self.buckets, "Select the bucket with the file")
+        datab = self.awaitResponse(client)
+
+        dirs = self.ls(self.buckets[datab.getData()][0])
+        self.sendMessage(client, 4, dirs, "Files")
+        self.sendMessage(client, 0)
+        
+
 
     def loadBuckets(self):
         dirs = glob.glob(self.path + '\*')
